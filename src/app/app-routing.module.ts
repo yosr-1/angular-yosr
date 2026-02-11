ae9161e283016f6dfb5687f64c5e08bf1ae6e404
yosr-1 <lenglizyosr1@gmail.com>
2026-02-11 15:16:52 +0100

    fix: correct import paths in app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,16 @@
-import { NgModule } from '@angular/core';
+﻿import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './core/home/home.component';
-import { ListSuggestionComponent } from './features/suggestions/list-suggestion.component';
-import { NotFoundComponent } from './core/notfound/notfound.component';
+import { ListSuggestionComponent } from './core/list-suggestion/list-suggestion.component';
+import { NotfoundComponent } from './core/notfound/notfound.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' }, // route par défaut
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'listSuggestion', component: ListSuggestionComponent },
   { path: 'suggestions', loadChildren: () => import('./features/suggestions/suggestions.module').then(m => m.SuggestionsModule) },
   { path: 'users', loadChildren: () => import('./features/users/users.module').then(m => m.UsersModule) },
-  { path: '**', component: NotFoundComponent } // route pour 404
+  { path: '**', component: NotfoundComponent }
 ];
 
 @NgModule({
